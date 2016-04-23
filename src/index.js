d4a1264611f975ef5d318344ec0b78e0d9398c4c
Guillaume Grossetie <ggrossetie@gmail.com>
2016-04-23 17:05:06 +0200

    Initialize project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var Opal = require('opal-runtime').Opal;
+require('./opal-parser.js');
+require('./opal-builder.js');
+
+Opal.require('nodejs');
+Opal.require('opal-builder');
+
+module.exports.Builder = ((Opal.get('Opal')).$$scope.get('Builder'));
