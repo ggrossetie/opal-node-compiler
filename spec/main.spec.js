1ef7665aafb66801998069d23f9cc52ef7bdac04
Guillaume Grossetie <ggrossetie@gmail.com>
2018-02-11 16:45:44 +0100

    Opal compiler should be able to compile a simple library

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -12,9 +12,17 @@ describe('Opal node compiler', function () {
       expect(Builder).not.toBe(null);
     });
 
-    it('builder should compile simple hello world', function() {
+    it('builder should compile a simple hello world', function() {
       var builder = Builder.create();
       var result = builder.build('spec/fixtures/hello.rb');
+      expect(result.toString()).toMatch(/self\.\$puts\("Hello world"\)/);
+    });
+
+    it('builder should compile a simple hello world library', function() {
+      var builder = Builder.create();
+      builder.appendPaths('spec/fixtures/hello-ruby/lib');
+      builder.appendPaths('src/stdlib');
+      var result = builder.build('hello');
       expect(result.toString()).toMatch(/self\.\$puts\("Hello world"\)/);
     });
   });
