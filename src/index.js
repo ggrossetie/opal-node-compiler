ac723905760f29ac7d82489106eec1bf48c8b861
Guillaume Grossetie <ggrossetie@gmail.com>
2020-05-16 16:56:34 +0200

    Upgrade development dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-var Opal = require('opal-runtime').Opal;
-require('./opal-builder.js');
-require('./opal-source-maps.js');
+var Opal = require('opal-runtime').Opal
+require('./opal-builder.js')
+require('./opal-source-maps.js')
 
-Opal.require('nodejs');
-Opal.require('opal-builder');
-Opal.require('opal-source-maps');
+Opal.require('nodejs')
+Opal.require('opal-builder')
+Opal.require('opal-source-maps')
 
 /**
  * Convert a JSON to an (Opal) Hash.
@@ -12,47 +12,47 @@ Opal.require('opal-source-maps');
  */
 var toHash = function (object) {
   if (object && !object.smap) {
-    return Opal.hash(object);
+    return Opal.hash(object)
   }
-  return object;
-};
+  return object
+}
 
-var Builder = Opal.const_get_qualified(Opal.const_get_relative([], 'Opal'), 'Builder');
-var ERB = Opal.const_get_qualified(Opal.const_get_relative([], 'Opal'), 'ERB');
+var Builder = Opal.const_get_qualified(Opal.const_get_relative([], 'Opal'), 'Builder')
+var ERB = Opal.const_get_qualified(Opal.const_get_relative([], 'Opal'), 'ERB')
 
 // Public API
 
-Builder['$$class'].prototype.create = function () {
-  return this.$new();
-};
-
-Builder.prototype.appendPaths = function (paths) {
-  this.$append_paths(paths);
-};
-
-Builder.prototype.setCompilerOptions = function (options) {
-  this.compiler_options = toHash(options);
-};
-
-Builder.prototype.build = function (path, options) {
-  return this.$build(path, toHash(options));
-};
-
-Builder.prototype.buildString = function (str, path = '.', options) {
-  return this.$build_str(str, path, toHash(options));
-};
-
-Builder.prototype.toString = function () {
-  return this.$to_s();
-};
-
-Builder.prototype.getSourceMap = function () {
-  return this.$source_map();
-};
-
-ERB['$$class'].prototype.compile = function (source, fileName) {
-  return this.$compile(source, fileName);
+Builder.$$class.prototype.create = function () {
+  return this.$new()
 }
 
-module.exports.Builder = Builder;
-module.exports.ERB = ERB;
+Builder.prototype.appendPaths = function (paths) {
+  this.$append_paths(paths)
+}
+
+Builder.prototype.setCompilerOptions = function (options) {
+  this.compiler_options = toHash(options)
+}
+
+Builder.prototype.build = function (path, options) {
+  return this.$build(path, toHash(options))
+}
+
+Builder.prototype.buildString = function (str, path = '.', options) {
+  return this.$build_str(str, path, toHash(options))
+}
+
+Builder.prototype.toString = function () {
+  return this.$to_s()
+}
+
+Builder.prototype.getSourceMap = function () {
+  return this.$source_map()
+}
+
+ERB.$$class.prototype.compile = function (source, fileName) {
+  return this.$compile(source, fileName)
+}
+
+module.exports.Builder = Builder
+module.exports.ERB = ERB
